ab16dd75dd7dca8b5c0aabc8858ca0964afee04e
Shivam Palaskar <shivampalaskar@gmail.com>
2020-01-29 22:58:34 +0530

    Commit7

---
+++ PreAssign_IACSD_Feb20_Q_7/src/PreAssign_IACSD_Feb20_Q_7.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : PreAssign_IACSD_Feb20_Q_7.c
+ Author      : Shivam Palaskar
+ Version     :
+ Copyright   : Open source
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts(""); /* prints  */
+	return EXIT_SUCCESS;
+}
